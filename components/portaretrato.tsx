687f35ceee0d110f6a9953580814b0f33325f61c
Franco <fganga@ymail.com>
2021-04-08 20:00:43 -0300

    avance con estilo de retratos.

--- components/portaretrato.tsx
+++ components/portaretrato.tsx
@@ -1,6 +1,7 @@
 import { Box, Flex } from '@chakra-ui/react';
 import { Image } from "@chakra-ui/react";
 
+import styles from '../styles/index.module.css';
 
 function getRandomArbitrary(min, max) {
   return Math.random() * (max - min) + min;
@@ -9,10 +10,17 @@ function getRandomArbitrary(min, max) {
 export default function Portaretrato() {
   const random = Math.floor(getRandomArbitrary(1,4));
   const path = `/assets/images/${random}.jpg`
-  console.log(path);
+  const name = `Hikari ${random}`;
   return (
-    <Flex boxSize="sm" align="center" justify="center">
-      <Image src={path} alt="me" width="80%" height="80%" />
+    <Flex w="400px" h="500px" align="center" justify="center">
+      <Box className={styles.retratoContainer} w="90%" h="90%">
+        <Image src={path} alt="me" width="100%" height="100%" />
+        <Box className={styles.overlay}>
+          <Box className={styles.text}>
+          {name}
+          </Box>
+        </Box>
+      </Box>
     </Flex>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import { Link, Grid, Box, Flex, Center } from '@chakra-ui/react';
+import { Link, Grid, Box, Flex, Center, Container } from '@chakra-ui/react';
 import Header from '../components/header';
 import Image from 'next/image';
 import Portaretrato from '../components/portaretrato';
@@ -16,11 +16,11 @@ export default function Home() {
       <Grid templateRows="auto 1fr auto" h="100%" gap={3}>
         <Header/>
         <Box w="100%">
-          <Center>
-              <Flex w={[300, 400, 560, "100%"]} flexWrap="wrap" border="1px" borderColor="red.200" justify="center">
-                {items.map((i, idx) => <Portaretrato key={idx}/>) }
-              </Flex>
-          </Center>
+          <Container maxW="container.xl">
+            <Flex wrap="wrap" justify="space-around">
+               {items.map((_, idx) => <Portaretrato key={idx}/>) }
+            </Flex>
+          </Container>
         </Box>
         <Box w="100%" bg="blue.500">
           <p>asd</p>
